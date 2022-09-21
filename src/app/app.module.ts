c8cda3da12349f136bf474a0e6de11b9e0cf6f36
RaCode75 <racode75@gmail.com>
2022-09-21 18:52:23 -0300

    dummy data and basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store/store.component';
+import { StoreModule } from './store/store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/store/store.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
+
+@Component({
+  selector: 'app-store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.scss']
+})
+export class StoreComponent{
+
+  constructor(private repository: ProductRepository) { }
+
+  get products(): Product[]{
+    return this.repository.getProducts();
+  }
+
+  get categories(): string[]{
+    return this.repository.getCategories();
+  }
+
+}
