ee39e32aa9ff346f2ebe429eb12e00c72eb58597
RaCode75 <racode75@gmail.com>
2022-09-22 17:55:56 -0300

    data binding...ok

--- src/app/store/store/store.component.ts
+++ src/app/store/store/store.component.ts
@@ -8,15 +8,20 @@ import { ProductRepository } from 'src/app/model/product.repository';
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent{
+  selectedCategory: string | undefined;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[]{
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[]{
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string){
+    this.selectedCategory = newCategory;
   }
 
 }
